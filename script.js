8b0fc7a090cad7ffe70d4785e51526ceda8e8cd2
UserTriple0 <52843987+UserTriple0@users.noreply.github.com>
2019-07-13 21:19:43 +1200

    Update and rename script.js.txt to script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-  function toggle_comments(box_id) {
+function toggle_comments(box_id) {
   var x = document.getElementById("comments-"+box_id);
   if (x.style.display === "none") {
     x.style.display = "inline-block";
@@ -30,10 +30,10 @@
     document.getElementById("posts").innerHTML += `<div id='linkbox-`+id+`' class="linkbox">
             <div id='linkbox_votes-`+id+`' class="linkbox_votes">
                 <span id="upvoteLabel-`+id+`">`+upvotes+`</span>
-                <a href="#"><img src="images/legit.png onclick="upvote(`+id+`)" title="Legit" id="upvoteButton-`+id+`"></a>
+                <img src="upvote.png" onclick="upvote(`+id+`)" id="upvoteButton-`+id+`">
                 <span id="totalLabel-`+id+`">`+(Number(upvotes)-Number(downvotes)).toString()+`</span>
-                <a href="#"><img src="images/bull.png" onclick="downvote(`+id+`)" title="Smells like bullcrap" id="downvoteButton-`+id+`"></a><span id="downvoteLabel-`+id+`">`+upvotes+`</span>
-                  <span id="downvotesLabel-`+id+`">`+downvotes+`</span>
+                <img src="downvote.png" onclick="downvote(`+id+`)" id="downvoteButton-`+id+`">
+                <span id="downvotesLabel-`+id+`">`+downvotes+`</span>
             </div>
             <div class="linkbox_main">
                 <div class="linkbox_titlebox">
@@ -46,24 +46,22 @@
                     <a class="linkbox_discuss_button" id="linkbox_discuss_button-`+id+`" onclick="toggle_comments(`+id+`)" href="#">Discuss</a>
                     <!-- <a class="linkbox_sources_button">Sources</a> -->
                 </div>
+                <div class="linkbox_time">`+'SAMMY BOI AGE HERE'+`</div>
             </div>
         </div>
 
         <div class="commentsbox" id="comments-`+id+`">
                    <div class="commentsbox_header">Comments</div>
+                   <div class="comment_entry_container">
+                        <textarea placeholder="Join the discussion!" id="comment_entry_0" class="comment_entry"></textarea>
+                        <button class="comment_post" onclick="SAMMY BOI COMMENT BUTTON HERE" id="comment_post_0">Post</button>
+                    </div>
                    <div class="comments_container">
                        <div class="comment">
                            <span class="username">Sample Username</span><br>
-                           <span class="comment_text">Sample comment asdf;oawejf;oiasjd;foajsd;ofjiaw;oe</span>
-                       </div>
-
-                       <div class="comment">
-                           <span class="username">Sample Username</span><br>
-                           <span class="comment_text">Sample comment asdf;oawejf;oiasjd;foajsd;ofjiaw;oe</span>
-                       </div>
-
-                       <div class="comment">
-                           <span class="username">Sample Username</span><br>
+                           <div class="comment_date">
+                                SAMMY BOI COMMENT AGE HERE
+                            </div>
                            <span class="comment_text">Sample comment asdf;oawejf;oiasjd;foajsd;ofjiaw;oe</span>
                        </div>
                    </div>
